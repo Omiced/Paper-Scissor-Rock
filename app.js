a37886ceb43ee2d8647fa942af2dd8c7ac73e1c7
Josue Cano <jdevelopcano@gmail.com>
2021-06-08 18:40:45 -0500

    Agregado un evento se elimino un archivo se agrego un evento al btn piedra

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
-function computerPlay(){
-    let options = ["rock", "paper", "scissor"];
-    let word = options[Math.floor(Math.random() * options.length)];
-    return word;
+//variables y caching the dom
+let userScore = 0;
+let pcScore = 0;
+const humanScore_span = document.getElementById("humanScore");
+const pcScore_span = document.getElementById("pcScore");
+const piedra_btn = document.getElementById("btnPiedra");
+const papel_btn = document.getElementById("btnPapel");
+const tijera_btn = document.getElementById("btnTijera");
+
+//events
+piedra_btn.addEventListener("click", function () {
+  console.log(playRound("rock", computerPlay()));
+  if (userScore == 4) {
+    document.write("Ganaste el juego");
+  }
+});
+function computerPlay() {
+  let options = ["rock", "paper", "scissor"];
+  let word = options[Math.floor(Math.random() * options.length)];
+  return word;
 }
 
-function playRound(playerSelection, computerSelection){
-    let userSelection =  playerSelection.toLowerCase();
-    let computer =  computerSelection;
+function playRound(playerSelection, computerSelection) {
+  let userSelection = playerSelection;
+  let computer = computerSelection;
 
-    if(computer == userSelection){
-        return "It's a draw, they both chose the same thing";
-    }
-    else if(computer == "rock" && userSelection == "paper"){
-        return "You win!, paper beats rock.";
-    }
-    else if(computer == "paper" && userSelection == "scissor"){
-        return "You win!, scissor beats paper";
-    }
-    else if(computer == "scissor" && userSelection == "rock"){
-        return "You win!, rock beats scissor";
-    }
-    else{
-        return "You lose!, " + computer + " beats " + userSelection;
-    }
+  if (computer == userSelection) {
+    return "It's a draw, they both chose the same thing";
+  } else if (computer == "rock" && userSelection == "paper") {
+    return userScore++;
+  } else if (computer == "paper" && userSelection == "scissor") {
+    return userScore++;
+  } else if (computer == "scissor" && userSelection == "rock") {
+    return userScore++;
+  } else {
+    return pcScore++;
+  }
 }
-
-function game(){
-    let i = 0;
-    while(i <= 4){
-        i ++;
-        const user = prompt("Write your chose, rock, scissor or paper");
-        const pc = computerPlay();
-        console.log(playRound(user, pc));
-    }
-}
-
-game();

00a501ff0f0e52a66da804c9e700912cc72dcb3b
Josue Cano <jdevelopcano@gmail.com>
2021-06-10 20:10:14 -0500

    terminada version versión nueva con una interfaz sencilla para entrenar manipulación del DOM

--- app.js
+++ app.js
@@ -1,38 +1,89 @@
-//variables y caching the dom
 let userScore = 0;
 let pcScore = 0;
-const humanScore_span = document.getElementById("humanScore");
-const pcScore_span = document.getElementById("pcScore");
+let userChoice = "";
+let pcChoice = "";
+const humanScore_span = document.querySelector("#humanScore");
+const pcScore_span = document.querySelector("#pcScore");
 const piedra_btn = document.getElementById("btnPiedra");
-const papel_btn = document.getElementById("btnPapel");
+const papel_btn = document.querySelector("#btnPapel");
 const tijera_btn = document.getElementById("btnTijera");
+const mensaje_p = document.getElementById("mensajeResultado");
 
-//events
-piedra_btn.addEventListener("click", function () {
-  console.log(playRound("rock", computerPlay()));
-  if (userScore == 4) {
-    document.write("Ganaste el juego");
+function gana() {
+  userScore++;
+  humanScore_span.innerHTML = userScore;
+  mensaje_p.innerText = `Tu ganas ${userChoice} vence ${pcChoice}`;
+}
+
+function pierde() {
+  pcScore++;
+  pcScore_span.innerHTML = pcScore;
+  mensaje_p.innerText = `La pc gana ${pcChoice} vence ${userChoice}`;
+}
+
+function finJuego() {
+  if (userScore == 5) {
+    userScore = 0;
+    pcScore = 0;
+    pcScore_span.innerHTML = pcScore;
+    humanScore_span.innerHTML = userScore;
+    return (mensaje_p.innerText = "Fin del juego tu ganas con 5 puntos");
+  } else if (pcScore == 5) {
+    userScore = 0;
+    pcScore = 0;
+    pcScore_span.innerHTML = pcScore;
+    humanScore_span.innerHTML = userScore;
+    return (mensaje_p.innerText = "La pc gana con 5 puntos F");
   }
-});
+}
+
 function computerPlay() {
-  let options = ["rock", "paper", "scissor"];
+  let options = ["piedra", "papel", "tijeras"];
   let word = options[Math.floor(Math.random() * options.length)];
   return word;
 }
 
 function playRound(playerSelection, computerSelection) {
-  let userSelection = playerSelection;
-  let computer = computerSelection;
-
-  if (computer == userSelection) {
-    return "It's a draw, they both chose the same thing";
-  } else if (computer == "rock" && userSelection == "paper") {
-    return userScore++;
-  } else if (computer == "paper" && userSelection == "scissor") {
-    return userScore++;
-  } else if (computer == "scissor" && userSelection == "rock") {
-    return userScore++;
+  if (playerSelection == computerSelection) {
+    return (mensaje_p.innerHTML = "Empate");
+  } else if (computerSelection == "piedra" && playerSelection == "papel") {
+    return gana();
+  } else if (computerSelection == "papel" && playerSelection == "tijeras") {
+    return gana();
+  } else if (computerSelection == "tijeras" && playerSelection == "piedra") {
+    return gana();
   } else {
-    return pcScore++;
+    return pierde();
   }
 }
+function main() {
+  papel_btn.addEventListener("click", function () {
+    userChoice = "papel";
+    pcChoice = computerPlay();
+    if (userScore == 5 || pcScore == 5) {
+      finJuego();
+    } else {
+      playRound(userChoice, pcChoice);
+    }
+  });
+  piedra_btn.addEventListener("click", function () {
+    userChoice = "piedra";
+    pcChoice = computerPlay();
+    if (userScore == 5 || pcScore == 5) {
+      finJuego();
+    } else {
+      playRound(userChoice, pcChoice);
+    }
+  });
+  tijera_btn.addEventListener("click", function () {
+    userChoice = "tijeras";
+    pcChoice = computerPlay();
+    if (userScore == 5 || pcScore == 5) {
+      finJuego();
+    } else {
+      playRound(userChoice, pcChoice);
+    }
+  });
+}
+
+main();
